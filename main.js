47cf13f6a57d7f660428562a8d14349d8bc0c171
Nickolas Eduardo <nickolasvieira.el@gmail.com>
2022-08-08 21:07:49 -0300

    Show more button add | missing transition and close action

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const booksButton = document.getElementById('booksButton')
+let state = false
 
 booksButton.onclick = function() {
+    state = true
+
+    if (state == true) {
+        handleState()
+    } else {
+        alert('200')
+    }
+}
+
+function handleState() {
     const booksList = document.getElementById('booksList')
     booksList.classList.remove('hiddenBooks')
 }
